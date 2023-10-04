f1883dc9cbf10df321fb71da329153f764a1cefc
repco <repco@EXPLOJR5>
2023-10-04 16:13:12 -0600

    carpetas para las vistas de cada tabla

--- src/app/Services/rol.service.ts
+++ src/app/Services/rol.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+// importaciones para consumir la api
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../Interfaces/response-api';
+import { Rol } from '../Interfaces/rol';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RolService {
-
-  constructor() { }
+  constructor() {}
 }
--- src/app/Services/usuario.service.ts
+++ src/app/Services/usuario.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 // importaciones para consumir la api
-import {HttpClient} from"@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ResponseApi } from '../Interfaces/response-api';
 import { Usuario } from '../Interfaces/usuario';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioService {
-
-  constructor() { }
+  constructor() {
+    //aqui empieza el contructor
+  }
 }
