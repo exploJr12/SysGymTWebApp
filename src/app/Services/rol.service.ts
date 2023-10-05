d1a7e186a8daa5fe6ece0932a337d139cb6c7a81
repco <repco@EXPLOJR5>
2023-10-05 16:52:12 -0600

    capertas de la web

--- src/app/Services/rol.service.ts
+++ src/app/Services/rol.service.ts
@@ -1,3 +1,5 @@
+// src/app/services/rol.service.ts
+
 import { Injectable } from '@angular/core';
 // importaciones para consumir la api
 import { HttpClient } from '@angular/common/http';
@@ -5,11 +7,14 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ResponseApi } from '../Interfaces/response-api';
 import { Rol } from '../Interfaces/rol';
+//import { ResponseApi } from '../interfaces/response-api';
+//import { Rol } from '../interfaces/rol';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RolService {
   private urlApi: string = environment.endpoint + 'rol/';
-  constructor() {}
+
+  constructor(private http: HttpClient) {}
 }
