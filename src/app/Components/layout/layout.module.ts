4536ea03c157913cd86d8de6dabb3811472c7bd9
repco <repco@EXPLOJR5>
2023-10-05 19:05:37 -0600

    components routing

--- src/app/Components/layout/layout.module.ts
+++ src/app/Components/layout/layout.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
-import { DashBoardComponent } from './Pages/dash-board/dash-board.component';
-import { RolComponent } from './Pages/rol/rol.component';
-
+import { DashBoardComponent } from './pages/dash-board/dash-board.component';
+import { RolComponent } from './pages/rol/rol.component';
 
 @NgModule({
-  declarations: [
-    DashBoardComponent,
-    RolComponent
-  ],
-  imports: [
-    CommonModule,
-    LayoutRoutingModule
-  ]
+  declarations: [DashBoardComponent, RolComponent],
+  imports: [CommonModule, LayoutRoutingModule],
 })
-export class LayoutModule { }
+export class LayoutModule {}
